9146b0cf10f25f794da5ee27782252b0e8c94124
shalujha <shalujha219999@gmail.com>
2019-04-19 21:17:23 +0530

    Sys Module

--- PythonModules.py
+++ PythonModules.py
@@ -5,7 +5,7 @@ print(sys.argv)
 # sum()
 print(sys.version)
 # InterPreter will search for Package in theses Path
-print(sys.path)
-print(sys.maxsize)
-print(sys.stdin)
+print(sys.path) 
+print(sys.maxsize) # it returns the maximum size of the data structure that the system can hold. it can varies from system to system
+print(sys.stdin)  # stdin and stdout are file system objects which are used for input and output data
 print(type(sys.stdout))
