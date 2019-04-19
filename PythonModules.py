07ea1345de55e61b9443b9606d312dfda476bce5
shalujha <shalujha219999@gmail.com>
2019-04-19 21:17:07 +0530

    Module

---
+++ PythonModules.py
@@ -0,0 +1,11 @@
+import sys
+print(sys.argv)
+# def sum():
+  #  print(int(sys.argv[1])+int(sys.argv[2]))
+# sum()
+print(sys.version)
+# InterPreter will search for Package in theses Path
+print(sys.path)
+print(sys.maxsize)
+print(sys.stdin)
+print(type(sys.stdout))
