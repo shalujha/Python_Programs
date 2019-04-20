cd8e0cce3f443fd655eb1f8c89589010dda7a0cb
shalujha <shalujha219999@gmail.com>
2019-04-20 12:17:39 +0530

    Module

---
+++ test.py
@@ -0,0 +1,7 @@
+#import Hello
+# Hello.sayHello("Donald","Trumph")
+# Hello.sayHi("shalini")
+# ********************Another Method******************************
+from Hello import *
+sayHello("Donald","Trumph")
+sayHi("shalini")
