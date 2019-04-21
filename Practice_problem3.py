97ff79ac16dd3862cd01a044bf497c89f2b4cdbc
shalujha <shalujha219999@gmail.com>
2019-04-21 16:37:40 +0530

    Q3

---
+++ Practice_problem3.py
@@ -0,0 +1,7 @@
+import random
+l=input("enter any elements in a list")
+list=list(l)
+# random.shuffle(list)
+# print(list)
+# printing last 3 elements of a list without using for loop
+print(list[-3:])
