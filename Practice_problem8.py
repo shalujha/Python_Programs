06c78fabbb49b6f856b1ebed6a921850ea820907
shalujha <shalujha219999@gmail.com>
2019-04-21 16:41:29 +0530

    Q8

---
+++ Practice_problem8.py
@@ -0,0 +1,13 @@
+n=int(input("enter the number of rows"))
+row=1
+nst=1
+while row<=n:
+    cst=1
+    while cst<=nst:
+        print("@",end='')
+        cst+=1
+    print()
+    nst+=1
+    row+=1
+
+
