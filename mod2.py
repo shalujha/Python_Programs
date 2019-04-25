2046c66806b96042c094f4156e0d8205ddaf6f57
shalujha <shalujha219999@gmail.com>
2019-04-25 15:18:06 +0530

    mod2

---
+++ mod2.py
@@ -0,0 +1,9 @@
+import mod1
+def sayHello():
+    print("Hello")
+mod1.sayHi()
+if(__name__==__main__):
+        # do some work when it is not executed directly
+    print("mod2 not Excuted Directly")
+else:
+    print("mod2 is executed directly")
