41d06660dc84a41da588ecb66713d0f831dad880
shalujha <shalujha219999@gmail.com>
2019-04-25 15:17:40 +0530

    mod1

---
+++ mod1.py
@@ -0,0 +1,7 @@
+def sayHi():
+    print("Hi")
+if(__name__==__main__):
+        # do some work when it is not executed directly
+    print("mod1 not Excuted Directly")
+else:
+    print("mod 1 is executed directly")
